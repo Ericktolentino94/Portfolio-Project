e5373698fd9e7544b44ba76d8a70521997fb9ea9
Erick Tolentino <ericktolentino@pursuit.org>
2023-07-11 20:04:03 -0400

    added the api to the value

---
+++ planets/planet.js
@@ -0,0 +1,1 @@
+const base_url = "http://swapi.dev/api/people?page=2"
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const url = "http://swapi.dev/api/people?page=2"
 const section = document.querySelector(".section")
+const searchButton = document.querySelector("#submitButton")
+const searchInput = document.querySelector("#searchInput")
+
 
 window.addEventListener("DOMContentLoaded" , function getData () {
     fetch(url)
@@ -12,11 +15,24 @@ window.addEventListener("DOMContentLoaded" , function getData () {
         <h2>${json.results[i].name}</h2>
         <p><strong>Birth Year: </strong>${json.results[i].birth_year}</p>
         <p><strong>Hair Color: </strong>${json.results[i].hair_color}</P>
-        <button>Character Home Planet</button>`
+        <button value="${json.results[i].homeworld}">Character Home Planet</button>`
         const planetButton = document.querySelector("button")
+        
+        
         planetButton.addEventListener("click", function() {
-            window.location.href = "./planet.html";
+            const homePlanet = this.value
+            window.location.href = `./planets/planet.html?value=${homePlanet}`;
+            console.log(`${json.results[i].homeworld}`)
+            
         })
+        }
+        searchButton.addEventListener("click", function() {
+            let query = searchInput.value;
+            performSearch(query);
+        })
+
+        function performSearch(query) {
+            console.log(`Performing search for:`, query)
         }
     })
     .catch(error => {
