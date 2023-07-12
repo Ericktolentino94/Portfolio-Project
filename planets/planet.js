d53c09c8bc62d4bbcd4bff4547d4f0e873519372
Erick Tolentino <ericktolentino@pursuit.org>
2023-07-11 23:59:10 -0400

    fixed the buttons

--- planets/planet.js
+++ planets/planet.js
@@ -1,1 +1,30 @@
-const base_url = "http://swapi.dev/api/people?page=2"
+const planetInfo = document.getElementById("planetInfo")
+
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const planetUrl = urlParams.get("planet")
+
+    fetch(planetUrl)
+        .then(response => response.json())
+        .then(planet => {
+            const planetName = planet.name; 
+            const planetPopulation = planet.population;
+            const planetRotation = planet.rotation_period;
+            const planetOrbital = planet.orbital_period;
+            const planetClimate = planet.climate;
+            const planetGravity = planet.gravity
+            const planetTerrain = planet.terrain
+
+
+            const planetInfoHTML = `<h2>Planet: ${planetName}</h2>
+                                    <p> Population: ${planetPopulation}</p>
+                                    <p> Planet Rotation: ${planetRotation}</p>
+                                    <p> Orbital period: ${planetOrbital}</p>
+                                    <p> Climate: ${planetClimate}</p>
+                                    <p> Gravity: ${planetGravity}</p>
+                                    <p> Planet Terrain: ${planetTerrain}</p>`
+                 planetInfo.innerHTML = planetInfoHTML
+        })
+});
--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-const url = "http://swapi.dev/api/people?page=2"
-const section = document.querySelector(".section")
-const searchButton = document.querySelector("#submitButton")
-const searchInput = document.querySelector("#searchInput")
+const url = "http://swapi.dev/api/people?page=2";
+const section = document.querySelector("section");
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#searchButton");
+const form = document.querySelector('form');
 
 
-window.addEventListener("DOMContentLoaded" , function getData () {
-    fetch(url)
-    .then(data => data.json ())
-    .then(json => {
-        console.log(json)
 
-        for(i=0 ; i < json.results.length; i++) {
-        section.innerHTML += `<article class="card">
-        <h2>${json.results[i].name}</h2>
-        <p><strong>Birth Year: </strong>${json.results[i].birth_year}</p>
-        <p><strong>Hair Color: </strong>${json.results[i].hair_color}</P>
-        <button value="${json.results[i].homeworld}">Character Home Planet</button>`
-        const planetButton = document.querySelector("button")
-        
-        
-        planetButton.addEventListener("click", function() {
-            const homePlanet = this.value
-            window.location.href = `./planets/planet.html?value=${homePlanet}`;
-            console.log(`${json.results[i].homeworld}`)
-            
-        })
-        }
-        searchButton.addEventListener("click", function() {
-            let query = searchInput.value;
-            performSearch(query);
-        })
 
-        function performSearch(query) {
-            console.log(`Performing search for:`, query)
-        }
+window.addEventListener("DOMContentLoaded", function getData() {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      for (let i = 0; i < data.results.length; i++) {
+        section.innerHTML += `
+          <article class="card">
+            <h2>${data.results[i].name}</h2>
+            <p><strong>Birth Year: </strong>${data.results[i].birth_year}</p>
+            <p><strong>Hair Color: </strong>${data.results[i].hair_color}</p>
+            <button class="characterButton" value="${data.results[i].homeworld}">Character Home Planet</button>
+          </article>
+        `;
+      }
+
+      const planetButtons = document.querySelectorAll(".characterButton");
+      planetButtons.forEach(button => {
+        button.addEventListener("click", handlePlanetButtonClick);
+      });
     })
     .catch(error => {
-        console.error(`Error`, error);
+      console.error("Error:", error);
     });
-
 });
+
+function handlePlanetButtonClick(event) {
+  const homeworld = event.target.value;
+  const planetURL = `./planets/planet.html?planet=${encodeURIComponent(homeworld)}`;
+  window.location.href = planetURL;
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const searchInput = document.getElementById('searchInput').value;
+
+    
+    fetch(`"http://swapi.dev/api/people?page=2"${encodeURIComponent(searchInput)}`)
+      .then(response => response.json())
+      .then(data => {
+        // Handle API response
+        console.log('API Response:', data);
+        // Display or process the retrieved character information
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
